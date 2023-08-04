ae69d4fc9283713a9603d54a1cb69e908c113ed2
ehuitz <elmohuitz@gmail.com>
2023-08-04 03:00:24 +0200

    added dark mode class

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -3,10 +3,6 @@
     <div class="text-center">
       <p class="text-base font-semibold text-indigo-600">Notification System</p>
       <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">{{ $subject }}</h1>
-      <p class="mt-6 text-base leading-7 text-gray-600">{{ $content }}</p>
-      <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
-        <a href="#" class="text-sm font-semibold text-gray-900">Contact support <span aria-hidden="true">&rarr;</span></a>
-      </div>
+      <p class="mt-6 text-2xl leading-7 text-gray-600">{{ $content }}</p>
     </div>
   </div>
